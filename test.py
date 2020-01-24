3cb5a1bc4b0d701269b23c4aae17ed5f14f68759
Elie Gavoty <eliegavoty@free.fr>
2020-01-24 16:30:35 +0100

    fix

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ class TestHello(unittest.TestCase):
         name = 'Simon'
         rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
-        self.assertIn(bytearray("{name}", 'utf-8'), rv.data)
+        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
     def test_new_route(self):
         name = 'Simon'
